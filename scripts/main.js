258a3d22b54c5c6a292ba905f741140f939ec78d
Ariel <apchenet@gmail.com>
2020-10-25 09:48:10 +0100

    removing buggy function

--- scripts/main.js
+++ scripts/main.js
@@ -7,20 +7,11 @@ function setUsername() {
 	let name = prompt('Please enter your name');
 	localStorage.setItem('name', name);
 	heading.textContent = 'Welcome ' + name +' !';
-	if(!name) {
-    		setUsername();
-	}
+	
 
 }
-
-if (!localStorage.getItem('name')) {
-	setUsername();
-
-}
-else {
 	let storedName = localStorage.getItem('name');
 	heading.textContent = 'Welcome ' + name +' !';
-}
 
 button.onclick = function () {
 	setUsername();
